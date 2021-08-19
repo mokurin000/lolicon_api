2618218c52af6d1a8ecea93a2e62d146db6f2b14
poly000 <1348292515@qq.com>
2021-08-19 09:45:19 +0800

    add basic convert

--- src/request.rs
+++ src/request.rs
@@ -1,10 +1,18 @@
 use crate::Error;
 
-#[derive(Error, Copy, Clone)]
+#[allow(dead_code)]
+#[derive(Debug, Error, Copy, Clone)]
 enum LoliError {
     IlleagalNum,
-    IlleagalUid,
     IlleagalSize,
+    TooManyUid,
+}
+
+impl std::fmt::Display for LoliError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        write!(f, "{:?}", self)?;
+        Ok(())
+    }
 }
 
 #[derive(Clone, Debug)]
@@ -24,17 +32,85 @@ struct Request {
     /// proxy for `pixiv.net`, `i.pixiv.cat`, e.g.
     proxy: Option<String>,
     /// Only show artworks after this UNIX time in millisecond.
-    date_after: <u64>,
+    date_after: Option<u64>,
     /// Only show artworks before this UNIX time in millisecond.
-    date_before: <u64>,
+    date_before: Option<u64>,
     /// If this is `true`, some automatic convert between keywords and tags will be disabled.
     dsc: Option<bool>,
 }
 
-#[derive(Copy, Clone)]
+#[allow(dead_code)]
+#[derive(Copy, Clone, Debug)]
 pub enum R18 {
     NonR18,
     R18,
     Mixin,
 }
 
+#[allow(dead_code)]
+impl Request {
+
+    fn r18(mut self, r: R18) -> Self {
+        self.r18 = Some(r);
+        self
+    }
+
+    fn num(mut self, amount: u8) -> Result<Self, LoliError> {
+        match amount {
+            1..=100 => {
+                self.num = Some(amount);
+                Ok(self)
+            }
+            _ => {
+                Err(LoliError::IlleagalNum)
+            }
+        } 
+    }
+
+    fn uid(mut self, authors: Vec<u32>) -> Result<Self, LoliError> {
+        if authors.len() <= 20 {
+            self.uid = Some(authors);
+            Ok(self)
+        } else {
+            Err(LoliError::TooManyUid)
+        }
+    }
+
+    fn keyword(mut self, keyword: String) -> Self {
+        self.keyword = Some(keyword);
+        self
+    }
+
+}
+
+impl Into<String> for Request {
+    fn into(self) -> String {
+        let mut url: String = "https://api.lolicon.app/setu/v2?".into();
+        url += &self.r18.into_argument();
+        url
+    }
+}
+
+trait IntoArgument {
+    fn into_argument(&self) -> String;
+}
+
+impl IntoArgument for Option<R18> {
+    fn into_argument(&self) -> String {
+        if let Some(r) = self {
+            match r {
+                R18::NonR18 => {
+                    "&r18=0"
+                }
+                R18::R18 => {
+                    "&r18=1"
+                }
+                R18::Mixin => {
+                    "&r18=2"
+                }
+            }
+        } else {
+            "&r18=0"
+        } .into()
+    }
+}
--- src/response.rs
+++ src/response.rs
@@ -1,3 +1,5 @@
+#![allow(non_snake_case)]
+
 use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Serialize, Deserialize)]
